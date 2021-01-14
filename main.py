c841229d3154534779c4556a211fef7c35167265
tustxsfh <tustxsfh@gmail.com>
2021-01-14 23:13:26 +0800

    aaa

---
+++ main.py
@@ -0,0 +1,30 @@
+if __name__ == '__main__':
+
+    from sjxf_xwj import sjxx
+    # sjxx("15903500868", "c0Ps362i42AHqSsdJmiFlQ==")  #  薛文杰
+
+    from sjxf_psj import sjxx
+     # sjxx("13603502808", "Y0PSjIY/6D6L6FuQydijUw==")  #  彭素军
+
+
+    from sjxf_he import sjxx                           # 何蔚英
+
+    from sjxf_hou import sjxx                          # 侯亮
+
+    from sjxf_zhangyuan import sjxx                   #  张院长
+
+    from sjxf_lang import sjxx
+    # sjxx("15835660123", "kX6eFXtpoqk+IcZB9ISvig==")  #  朗丽英
+
+
+    from sjxf_zhangyu import sjxx
+
+    # sjxx("15835660106", "EhYV6jlQueYcKppFn3gsKA==")  #  张宇
+
+    from sjxf_zsm import sjxx
+    # sjxx("15835660201", "7E6oSiKYaFM2dYDRtSIQqQ==")  #  赵树民
+
+    from sjxf_jirong import sjxx                      #  季蓉
+
+    from fjw import sjxx                              #  冯俊玮
+
---
+++ sjxf_jirong.py
@@ -0,0 +1,278 @@
+import base64
+import json
+import time
+import requests
+
+articleList = json.loads(requests.get("http://221.204.170.88:8184/app/study/list_article/72?size=10&page=1").text)["data"]
+# print(articleList)
+
+#登录
+def login(username, password):
+    url = 'http://221.204.170.88:8184/app/user/login'
+    data = {"clientid": "234234", "deviceId": "id98fa4c9d-4db3-40e7-82e3-17218d29ec0e", "password": password,
+            "userName": username, }
+    headers = {
+        'Content-Type': 'application/json',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'okhttp/3.8.0'
+    }
+    response = requests.post(url, json=data, headers=headers)
+    userInfo = json.loads(response.text)['data']
+    info = json.loads(base64.b64decode(userInfo))
+    return [info['jwtToken'], info['id']]
+
+def do(Authorization, userId):
+    def get_appStartTime():
+        url = "http://221.204.170.88:8184/app/time"
+
+        headers = {
+                'Authorization': Authorization,
+                'sUserId': userId,
+                'Content-Type': 'application/json; charset=utf-8',
+                'Host': '221.204.170.88:8184',
+                'Connection': 'Keep-Alive',
+                'Accept-Encoding': 'gzip',
+                'User-Agent': 'okhttp/3.10.0',
+                'version' : '3.3.4'
+            }
+
+        Rsponse = requests.get(url, headers=headers)
+        app_start_time = json.loads(Rsponse.text)["data"]
+        # print(app_start_time)
+        return app_start_time
+
+    def read():
+
+        url = "http://221.204.170.88:8184/app/businessScore"
+
+        appStartTime = get_appStartTime()
+        # print(appStartTime)
+        payload = {'userId': userId,
+                   'time': '35',
+                   'type': '1',
+                   'articleId': articleList[3]["id"],
+                   'ifScore': '1',
+                   "appStartTime" : appStartTime
+                   }
+
+        headers = {
+            'Authorization': Authorization,
+            'sUserId': userId,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Host': '221.204.170.88:8184',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'okhttp/3.10.0',
+            'version': '3.3.4'
+        }
+        read_Rsponse = requests.post(url, headers=headers, json=payload)
+        # print("阅读文章====================" + read_Rsponse.text)
+    # read()
+    # read()
+
+
+    def stxx():
+        url = "http://221.204.170.88:8184/app/businessScore"
+
+        appStartTime = get_appStartTime()
+        # print(appStartTime)
+        payload = {'userId': userId,
+                   'time':'605',
+                   'type': '2',
+                   'articleId':'14',
+                   'ifScore':'1',
+                   "appStartTime" : appStartTime}
+        headers = {
+            'Authorization': Authorization,
+            'sUserId': userId,
+            'Content-Type': 'application/json; charset=utf-8',
+            'Host': '221.204.170.88:8184',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'okhttp/3.10.0',
+            'version': '3.3.4'
+        }
+        stRsponse = requests.post(url, headers=headers, json=payload)
+        # print("试听学习====================" + stRsponse.text)
+
+    # stxx()
+
+    def dati(str):
+        url = 'http://221.204.170.88:8184/app/questionLib'
+        pageData = {"page": 1, "pageSize": 10, "themeId": str}
+        headers = {
+            'Connection': 'keep-alive',
+            'Content-Length': '37',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'Authorization': Authorization,
+            'Origin': 'http://sxzhdjkhd.sxdygbjy.gov.cn:8081',
+            'User-Agent': 'Mozilla/5.0',
+            'Content-Type': 'application/json',
+            'Accept': '*/*',
+            'Referer': 'http://sxzhdjkhd.sxdygbjy.gov.cn:8081/zhdj-pre/',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,en-US;q=0.9',
+            'X-Requested-With': 'io.dcloud.H5B1841EE'
+        }
+        answerList = []
+        response = requests.post(url, json=pageData, headers=headers)
+        list = json.loads(response.text)
+        for data in list['data']['list']:
+            answerList.append({"selectAnswer": data['correctAnswer'], "grade": data['grade'], "ifCorrect": 1,
+                               "questionCode": data['code'], "questionType": data['type']})
+        uuidHeaders = {
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'Authorization': Authorization,
+            'Origin': 'http://sxzhdjkhd.sxdygbjy.gov.cn:8081',
+            'User-Agent': 'Mozilla/5.0',
+            'Accept': '*/*',
+            'Referer': 'http://sxzhdjkhd.sxdygbjy.gov.cn:8081/zhdj-pre/',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,en-US;q=0.9',
+            'X-Requested-With': 'io.dcloud.H5B1841EE'
+        }
+        uuidResponse = requests.get("http://221.204.170.88:8184/app/uuid", headers=uuidHeaders).text
+        uuidData = json.loads(uuidResponse)
+        uuid = '"' + uuidData['data'] + '"'
+        answer = {"userId": userId, "method": str, "totalGrade": "100", "list": answerList, "summaryCode": uuid}
+        answerHeaders = {
+            'Connection': 'keep-alive',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'Authorization': Authorization,
+            'Origin': 'http://sxzhdjkhd.sxdygbjy.gov.cn:8081',
+            'User-Agent': 'Mozilla/5.0',
+            'Accept': '*/*',
+            'Content-Type': 'application/json',
+            'Referer': 'http://sxzhdjkhd.sxdygbjy.gov.cn:8081/zhdj-pre/',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept-Language': 'zh-CN,en-US;q=0.9',
+            'X-Requested-With': 'io.dcloud.H5B1841EE'
+        }
+        dumps = json.dumps(answer)
+        # print(dumps)
+        answerResponse = requests.post("http://221.204.170.88:8184/app/question", data=dumps, headers=answerHeaders)
+        # print(answerResponse.text)
+        # print("完成答题" + str + answerResponse.text)
+
+    def scwz():
+        Headers = {
+            'Authorization': Authorization,
+            'User-Agent': 'san jin xian feng/3.2.6 (iPhone; iOS 13.4.1; Scale/3.00)',
+            'Accept-Language': 'zh-Hans-CN;q=1, ko-KR;q=0.9, en-US;q=0.8',
+            'sUserId': userId,
+        }
+        # 取消点赞
+        global articleList
+        articleList = \
+            json.loads(requests.get("http://221.204.170.88:8184/app/study/list_article/72?size=10&page=1").text)[
+                "data"]
+        unlovedata1 = {"type": "1", "userId": userId, "uniqueId": articleList[2]["id"]}
+        unlove1Response = requests.post("http://221.204.170.88:8184/app/loveCancelDelete", json=unlovedata1,
+                                        headers=Headers)
+        # print(unlove1Response.text)
+        unlovedata2 = {"type": "1", "userId": userId, "uniqueId": articleList[3]["id"]}
+        unlove2Response = requests.post("http://221.204.170.88:8184/app/loveCancelDelete", json=unlovedata2,
+                                        headers=Headers)
+        # print(unlove2Response.text)
+        # 取消收藏
+        uncollect1Response = requests.post("http://221.204.170.88:8184/app/collectCancelDelete", json=unlovedata1,
+                                           headers=Headers)
+        # print(uncollect1Response.text)
+        uncollect2Response = requests.post("http://221.204.170.88:8184/app/collectCancelDelete", json=unlovedata2,
+                                           headers=Headers)
+        # print(uncollect2Response.text)
+        headers = {
+            'Authorization': Authorization,
+            'User-Agent': 'okhttp/3.8.0',
+            'Content-Type': 'application/json',
+            'Host': '221.204.170.88:8184',
+            'sUserId': userId,
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        }
+        collectresponse1 = requests.post('http://221.204.170.88:8184/app/collect', json=unlovedata1, headers=headers)
+        # print(collectresponse1.text)
+        collectresponse2 = requests.post('http://221.204.170.88:8184/app/collect', json=unlovedata2, headers=headers)
+        # print(collectresponse2.text)
+        dzRsponse = requests.post(url='http://221.204.170.88:8184/app/love', json=unlovedata1, headers=headers)
+        # print(dzRsponse.text)
+        dzRsponse = requests.post(url='http://221.204.170.88:8184/app/love', json=unlovedata2, headers=headers)
+        # print(dzRsponse.text)
+ # 积分明细
+    def jifen():
+        url = 'http://221.204.170.88:8184/app/home/totayScore'
+
+        payload = {'userId': userId,
+                   'type': '2'
+                   }
+        headers = {
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache',
+            'Authorization': Authorization,
+            'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Redmi K20 Pro Premium Edition Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045224 Mobile Safari/537.36',
+            'Content-Type': 'application/json',
+            'Origin': 'http: // sxzhdjkhd.sxdygbjy.gov.cn: 8081',
+            'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Connection': 'Keep-Alive',
+            'X-Requested-With': 'io.dcloud.H5B1841EE',
+            'Accept-Encoding': 'gzip, deflate',
+            'Accept': '*/*',
+            'Referer': 'http: // sxzhdjkhd.sxdygbjy.gov.cn: 8081 / zhdj - pre / index.html',
+            'Host': '221.204.170.88:8184',
+        }
+        s = requests.session()
+        t = s.post(url, headers=headers, data=json.dumps(payload))
+        todayScore = json.loads(t.text)
+        if str(todayScore['data']['todayScore']) < '15.0':
+            global desp
+            desp = '【分值不够，请手动刷分!】季蓉今天增加了' + str(todayScore['data']['todayScore']) + '分，总分为' + str(
+                todayScore['data']['yearScore']) + '分'
+            print(desp)
+            # rizhi()
+        else:
+            desp = '季蓉今天增加了' + str(todayScore['data']['todayScore']) + '分，总分为' + str(
+                todayScore['data']['yearScore']) + '分'
+            print(desp)
+
+    # 答题
+    dati("1")
+    time.sleep(2)
+    dati("1")
+    time.sleep(2)
+    dati("4")
+    time.sleep(2)
+    dati("4")
+    time.sleep(2)
+    print("答题完毕")
+    # 试听学习
+    stxx()
+    time.sleep(2)
+    print("视听学习完毕")
+    # 收藏点赞
+    scwz()
+    time.sleep(2)
+    print("收藏点赞完毕")
+    # 阅读
+    read()
+    time.sleep(2)
+    read()
+    time.sleep(2)
+    print("文章学习完毕")
+    # 积分明细
+    time.sleep(5)
+    jifen()
+
+def sjxx(username, password):
+    JWT = login(username, password)
+    do('Bearer ' + str(JWT[0]), str(JWT[1]))
+
+
+username='15835660100'                         #季蓉的账号
+password ='Nnu/6slPtYPwroSFGhJgjQ=='           #季蓉的密码
+sjxx(username,password)
+time.sleep(10)
